5207900db2c00422a613c8c214f813497e8b6ec4
Vincent Thai <thai.vincent@tcs.com>
2021-03-30 13:34:48 -0400

    added form validation

--- src/components/Miles/Miles.js
+++ src/components/Miles/Miles.js
@@ -6,6 +6,7 @@ import "./Miles.css";
 function Miles({switchMilesAndRates, switchMilesAndHours}) {
   const dispatch = useDispatch();
   const [miles, setMiles] = useState('');
+  const [valid, setValid] = useState(true);
 
   const storeData = () => {
     dispatch(
@@ -13,18 +14,44 @@ function Miles({switchMilesAndRates, switchMilesAndHours}) {
     );
   };
 
+  const handleKeyUp = (e) => {
+    // Enter
+    if (e.keyCode === 13) {
+      if(miles === '') {
+        setValid(false);
+      } else {
+        storeData();
+        switchMilesAndHours();
+      }
+    }
+  }
+
+// storeData(); switchMilesAndHours();
   return (
     <div className="miles">
       <div className="miles_title">How many miles do you drive a year?</div>
-      <input 
+      {valid ? (
+        <input 
         className="miles_input" 
         value={miles} 
         onChange={e => setMiles(e.target.value)}
         placeholder="Enter miles"
-      />
+        onKeyUp={handleKeyUp}
+        />
+      ) : (
+        <>
+        <input 
+        className="miles_input" 
+        value={miles} 
+        onChange={e => setMiles(e.target.value)}
+        placeholder="Enter miles"
+        onKeyUp={handleKeyUp}
+        />
+        <div className="miles_invalid">Please enter miles driven</div>
+        </>
+      )}  
       <div className="miles_buttons">
         <button className="miles_button" onClick={() => switchMilesAndRates()}>Previous</button>
-        <button className="miles_button" onClick={() => {switchMilesAndHours(); storeData();}}>Next</button>
       </div>
     </div>
   )
