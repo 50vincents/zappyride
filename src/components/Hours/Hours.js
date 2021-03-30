47aa63cfd5b40749bd0ac9111142d0a0e2fa0339
Vincent Thai <thai.vincent@tcs.com>
2021-03-30 13:07:49 -0400

    full responsiveness

--- src/components/Hours/Hours.js
+++ src/components/Hours/Hours.js
@@ -27,11 +27,11 @@ function Hours({switchMilesAndHours, switchHoursAndCalc}) {
     <div className="hours">
       <div className="hours_title">What hours of the day do you plan to charge?</div>
       <div className="hours_buttons">
-        <button className="hours_button" onClick={() => setHours(1)}>Before noon</button>
-        <button className="hours_button" onClick={() => setHours(2)}>Between noon and 6 pm</button>
-        <button className="hours_button" onClick={() => setHours(3)}>After evening (6 pm)</button>
+        <button className="hours_button" onClick={() => setHours(1)}>Before Noon</button>
+        <button className="hours_button" onClick={() => setHours(2)}>Between Noon and 6 PM</button>
+        <button className="hours_button" onClick={() => setHours(3)}>After Evening (6 PM)</button>
       </div>
-      <button className="hours_button" onClick={() => switchMilesAndHours()}>Previous</button>
+      <button className="hours_previous" onClick={() => switchMilesAndHours()}>Previous</button>
     </div>
   )
 }
